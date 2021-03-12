6caf9a46c7c7793b846ed6dbb48d7d572c0e9fc7
Swapnil Pote <swapnil.p@genietalk.ai>
2021-03-13 02:28:41 +0530

    Added flask restful api with glove model only

--- ner_app/app/mod/api/views.py
+++ ner_app/app/mod/api/views.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
+
+from ..src.train import gen_data
+from ..src.predict import Prediction
 
 api = Blueprint("api", __name__)
+
+prediction = Prediction()
 
 
 @api.route("/")
 def index():
     return jsonify({"api": "Testing restful api"})
+
+
+@api.route("/predict", methods=["GET"])
+def predict():
+    x = request.json.get("x")
+
+    result = prediction.glove_predict(x)
+
+    return jsonify({"x": x, "result": " ".join([i for _, i in zip(x.split(), result)])})
--- ner_app/app/mod/package/utils/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-STATIC_PATH = "static"
-DATA_PATH = os.path.join(os.getcwd(), STATIC_PATH)
